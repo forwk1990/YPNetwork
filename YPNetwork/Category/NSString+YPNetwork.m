b91c33108e989386e64315fc13a2654ddd035488
forwk1990 <forwk1990@gmail.com>
2016-08-29 21:18:57 +0800

    manager \ proxy add weak refrence

---
+++ YPNetwork/Category/NSString+YPNetwork.m
@@ -0,0 +1,13 @@
+//
+//  NSString+YPNetwork.m
+//  YPNetworkDemo
+//
+//  Created by 尹攀 on 16/8/27.
+//  Copyright © 2016年 com.itachi. All rights reserved.
+//
+
+#import "NSString+YPNetwork.h"
+
+@implementation NSString (YPNetwork)
+
+@end
